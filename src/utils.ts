feed9e03f9693e91028434e8959a4917a80c85a5
Braden Napier <brady.napier@dashos.net>
2020-06-18 22:21:43 -0400

    fix: 🐛 also exclude the type only import in utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,11 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+// type-only import - fixed by https://github.com/benmosher/eslint-plugin-import/pull/1820
+// eslint-disable-next-line import/no-extraneous-dependencies
+import type { TSESTree } from '@typescript-eslint/experimental-utils';
 
 import * as eslint from 'eslint';
-import { TSESTree } from '@typescript-eslint/experimental-utils';
 
 export function createImportRuleListener(
   validateImport: (node: TSESTree.Literal, value: string) => any,
